78ed7277992e9b9fe5106e63d55dd2f73215cfe1
Ayeeta <elijate@gmail.com>
2019-05-28 04:08:31 +0300

    Insert into MS SQL Database

--- AccrualSystem/AccrualSystem/Form1.cs
+++ AccrualSystem/AccrualSystem/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AccrualSystem
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=DELL;Initial Catalog=AccrualDB;User ID=sa;Password=sap");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int num = 4;
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into employees values('"+txtEmployeeID.Text+"','"+txtEmployeeName.Text+"','"+dateStartDate.Text+"','"+cmbSeniority.Text+"', '"+num+"')";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            txtEmployeeID.Text = " ";
+            txtEmployeeName.Text = " ";
+            cmbSeniority.Text = " ";
+            
         }
     }
 }
