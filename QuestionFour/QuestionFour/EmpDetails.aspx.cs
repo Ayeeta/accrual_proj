e9e402a11c94ee5af456336d150b7aedb5023f7e
Ayeeta <elijate@gmail.com>
2019-06-18 11:29:42 +0300

    Save to database

--- QuestionFour/QuestionFour/EmpDetails.aspx.cs
+++ QuestionFour/QuestionFour/EmpDetails.aspx.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace QuestionFour
 {
     public partial class EmpDetails : System.Web.UI.Page
     {
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["QuestionsDBConnectionString"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtEmpDep.Text = txtEmpName.Text = txtEmpSalary.Text = "";
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into emps values('" + txtEmpName.Text + "','" + txtEmpDep.Text + "','" + txtEmpSalary.Text + "')", conn);
+            
+            cmd.ExecuteNonQuery();
+            
+            conn.Close();
+            txtEmpDep.Text = txtEmpName.Text = txtEmpSalary.Text = "";
         }
     }
 }
--- QuestionFour/QuestionFour/EmpDetails.aspx.designer.cs
+++ QuestionFour/QuestionFour/EmpDetails.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace QuestionFour {
         protected global::System.Web.UI.WebControls.TextBox txtEmpSalary;
         
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
         /// btnCancel control.
         /// </summary>
         /// <remarks>
@@ -65,5 +74,14 @@ namespace QuestionFour {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
